2b2fac87fd83020c9a54bf2400b47ceb32cb7079
Apoorvjaingithub <38976439+Apoorvjaingithub@users.noreply.github.com>
2020-01-24 22:45:57 +0530

    Add files via upload

---
+++ JenkinsCalculator.java
@@ -0,0 +1,14 @@
+package com.aj.training.jenkins;
+
+public class JenkinsCalculator {
+
+	public int addNumbers (int numberOne, int numberTwo)
+	{
+		return numberOne + numberTwo;
+	}
+	
+	public int substractNumbers(int numberOne, int numberTwo)	
+	{
+		return numberOne - numberTwo;
+	}
+}
